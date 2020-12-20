0845dda4e5ebb0440ec9d1cb9ad8d91903978904
jps723 <31663418+jps723@users.noreply.github.com>
2020-12-20 02:48:34 -0500

    updated to flex-single only

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
-    title: "Personal Site",
-    description: "Get to know me",
-    keywords: ["eleventy", "template", "simple", "clean"],
+    title: "Jesse Simpson",
+    description: "A portfolio and blog",
+    keywords: ["art", "music", "media", "tech", "developer", "hardware", "software"],
     // your site url without trailing slash
     url: "https://priceless-noyce-2f5931.netlify.app/",
     // how many posts you want to show for each page
