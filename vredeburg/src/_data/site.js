30b5ca660b323e03d6e867ad9983c4a5df5a3d7f
jps723 <31663418+jps723@users.noreply.github.com>
2020-12-19 23:57:41 -0500

    personalized and updated to 24 pagination

--- vredeburg/src/_data/site.js
+++ vredeburg/src/_data/site.js
@@ -5,7 +5,7 @@ module.exports = {
     // your site url without trailing slash
     url: "https://priceless-noyce-2f5931.netlify.app/",
     // how many posts you want to show for each page
-    paginate: 24,
+    paginate: 12,
     // if you want to add disqus to your site
     // disqusShortname: "your-shortname"
 }
