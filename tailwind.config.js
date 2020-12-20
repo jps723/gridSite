a4fa9036262dff40182c6d124a688b51d0e12cd6
jps723 <31663418+jps723@users.noreply.github.com>
2020-12-20 03:00:59 -0500

    made flex at 33.33333% for triple

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
             flex: {
                 "single": "0 0 100%",
                 "double": "0 0 50%",
-                "triple": "0 0 10%"
+                "triple": "0 0 33.333333%"
             },
             fontSize: {
                 "tiny": "0.925rem"
